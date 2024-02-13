a7e4d5a754f70b0fd5a9e87255a1770cb4cf8602
DKH <derek.hawryluk@edu.sait.ca>
2024-02-13 09:05:57 -0700

    file org

---
+++ script.js
@@ -0,0 +1,10 @@
+// create a const to select the body in HTML
+
+// use this const to select the element
+
+// toggle this class by selecting the function
+
+function dark_mode() {
+  const darkMode = document.body;
+  darkMode.classList.toggle("dark-mode");
+}
